7bce56dfb0320385c1c64be6ca8de0f1532bb5ce
jean1024 <1737923789@qq.com>
2019-02-12 14:43:32 +0800

    1

--- app.js
+++ app.js
@@ -2,16 +2,9 @@
 const Koa = require('koa')
 const app = new Koa()
 const wechat = require("./wechat/g")
-const config = {
-    wechat:{
-        appID: 'wx5e8a16fa067e5067',
-        appsecret: '73dcf94846cdad3831cf04de8baed56d',
-        tocken: 'qiuzhilin'
-    }
-}
+const config = require('./config')
 
 app.use(wechat(config))
-
 app.listen(80, () => {
     console.log('App listening on port 80!');
 });
--- wechat/g.js
+++ wechat/g.js
@@ -1,18 +1,17 @@
-'use strict'
 const sha1 = require('sha1')
-module.exports = function(opts){
-    return async function (ctx) {
-        const tocken = opts.wechat.tocken
-        const signature = ctx.query.signature
-        const echostr = ctx.query.echostr
-        const timestamp = ctx.query.timestamp
-        const nonce = ctx.query.nonce
-        const str = [tocken,timestamp,nonce].sort().join('')
+module.exports = function(config){
+    return async function(ctx){
+        var signature = ctx.query.signature,//微信加密签名
+            timestamp = ctx.query.timestamp,//时间戳
+            nonce = ctx.query.nonce,//随机数
+            echostr = ctx.query.echostr,//随机字符串
+            token = config.token;
+        const str = [token,nonce,timestamp].sort().join('')
         const sha = sha1(str)
         if(sha === signature){
             ctx.body = echostr
         }else{
-            ctx.body = '验证失败'
+            ctx.body = '验证失败,请在微信中打开'
         }
     }
 }
