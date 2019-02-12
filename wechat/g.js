17df1f9429fbfe0237b9566c1852d9d85b08611f
jean1024 <1737923789@qq.com>
2019-02-12 16:34:03 +0800

    3

--- wechat/g.js
+++ wechat/g.js
@@ -5,30 +5,30 @@ const getRowBody = require('raw-body')
 module.exports = function(opts){
     //微信加密
     var wechat = new Wechat(opts)
-    return function *(ctx){
-        var signature = ctx.query.signature,//微信加密签名
-            timestamp = ctx.query.timestamp,//时间戳
-            nonce = ctx.query.nonce,//随机数
-            echostr = ctx.query.echostr,//随机字符串
+    return function *(next){
+        var signature = this.query.signature,//微信加密签名
+            timestamp = this.query.timestamp,//时间戳
+            nonce = this.query.nonce,//随机数
+            echostr = this.query.echostr,//随机字符串
             token = opts.token;
         var str = [timestamp,token,nonce].sort().join('')
         var sha = sha1(str)
 
-        if(ctx.method === 'GET'){
+        if(this.method === 'GET'){
             if(sha === signature){
-                ctx.body = echostr
+                this.body = echostr
             }else{
-                ctx.body = '验证失败'
+                this.body = '验证失败'
             }
-        }else if(ctx.method === 'POST'){
+        }else if(this.method === 'POST'){
             if(sha !== signature){
-                ctx.body = '验证失败'
+                this.body = '验证失败'
                 return false
             }
-            var data = yield getRowBody(ctx.req, {
-                length: ctx.length,
+            var data = yield getRowBody(this.req, {
+                length: this.length,
                 limit: '1mb',
-                encoding: ctx.charset
+                encoding: this.charset
             })
             console.log(data.toString())
         }
