83e9d531b8c59b056867a12e4b465166e79fb845
jean1024 <1737923789@qq.com>
2019-02-12 17:05:16 +0800

    3

--- wechat/util.js
+++ wechat/util.js
@@ -14,6 +14,7 @@ exports.parseXMLAsync = function(xml){
 }
 function formatMessage(result){
     var message = {}
+    console.log(result)
     if(typeof result === 'object'){
         var keys = Object.keys(result)
         for (let i = 0; i < keys.length; i++) {
